b34866e0f845f9af762948b5327af1da4a6055c9
gabrielsilper <gabrielsilper.main@gmail.com>
2026-01-30 19:48:35 -0400

    feat: add Researcher entity and ResearcherDegree enum for researcher management

---
+++ src/entities/researcher.ts
@@ -0,0 +1,44 @@
+import { ResearcherDegree } from 'enums/reseacher-degree';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('researchers')
+export default class Researcher {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'varchar', nullable: false, unique: true })
+  registration!: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  name!: string;
+
+  @Column({ type: 'varchar', nullable: false, unique: true })
+  email!: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  password!: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  specialty!: string;
+
+  @Column({ type: 'int', enum: ResearcherDegree, nullable: false })
+  degree!: ResearcherDegree;
+
+  @Column({ type: 'varchar', nullable: true })
+  research!: string;
+
+  @Column({ type: 'date', nullable: false })
+  birthDate!: Date;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
---
+++ src/enums/reseacher-degree.ts
@@ -0,0 +1,7 @@
+export enum ResearcherDegree {
+  INDEFINIDO,
+  GRADUACAO,
+  ESPECIALIZACAO,
+  MESTRADO,
+  DOUTORADO,
+}
